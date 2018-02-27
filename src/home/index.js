b7a698d4d398c6ac8526389d61977778b4aeb9e5
Jim Medlock <jdmedlock@gmail.com>
2018-02-27 11:35:36 -0600

    feature: Add makeGrid() method to Grid class
    Add the makeGrid() method to the Grid class to begin testing of the creation and rendering of the design grid.
    
    Resolves: N/a
    See also: https://github.com/jdmedlock/pixelartmaker/issues/2

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import Grid from '../classes/Grid';
 
 let designGrid = null;
@@ -7,6 +5,9 @@ let designGrid = null;
 $(document).ready(function() {
   console.clear();
   designGrid = new Grid(8, 8);
+
+  console.log(designGrid);
+  $( ".design-grid-wrapper" ).append( designGrid.makeGrid() );
 
   // Create a button handler for the on/off button
   $("#grid-row-decrement").click((event) => {
